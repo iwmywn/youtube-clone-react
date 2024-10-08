12e2403af3adf643d92f6827466bbae31590f3e0
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-08 10:44:05 +0700

    add dot icon

--- src/data/icons.tsx
+++ src/data/icons.tsx
@@ -826,3 +826,25 @@ export const sendFeedback = {
     </svg>
   ),
 };
+
+export const more = {
+  name: "More",
+  svg: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 24 24"
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+      focusable="false"
+      aria-hidden="true"
+      style={{
+        pointerEvents: "none",
+        display: "inherit",
+        fill: color,
+      }}
+    >
+      <path d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z" />
+    </svg>
+  ),
+};
